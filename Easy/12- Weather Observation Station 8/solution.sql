c77eb9a0c569397ea534409fd8cdf2e842756194
Ahmed-Hany-saad <105203723+Ahmed-Hany-saad@users.noreply.github.com>
2024-08-11 22:03:15 +0300

    Update solution.sql

--- Easy/12- Weather Observation Station 8/solution.sql
+++ Easy/12- Weather Observation Station 8/solution.sql
@@ -3,4 +3,4 @@ From STATION
 where (CITY like "%a" or CITY like "%e" or CITY like "%i" or CITY like "%o" or CITY like "%u")
 AND (CITY like"a%" or CITY like "e%" or CITY like "i%" or CITY like "o%" or CITY like "u%");
 
--- WHERE CITY REGEXP '^[AEIOU].*[aeiou]$'; the same result
+-- WHERE CITY REGEXP '^[aeiou].*[aeiou]$'; the same result
