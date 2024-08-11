b72947d5588fec2fc3fbbb68f24ad6588aa2422e
Ahmed-Hany-saad <105203723+Ahmed-Hany-saad@users.noreply.github.com>
2024-08-11 21:54:18 +0300

    Add files via upload

---
+++ Easy/12- Weather Observation Station 8/solution.sql
@@ -0,0 +1,5 @@
+Select DISTINCT CITY 
+From STATION
+where (CITY like "%a" or CITY like "%e" or CITY like "%i" or CITY like "%o" or CITY like "%u")
+AND (CITY like"a%" or CITY like "e%" or CITY like "i%" or CITY like "o%" or CITY like "u%");
+
