a041b43b3b3fc2a7d9315285524b309ed15ce9b1
Ahmed-Hany-saad <105203723+Ahmed-Hany-saad@users.noreply.github.com>
2024-08-11 22:25:43 +0300

    Add files via upload

---
+++ Easy/13- Weather Observation Station 9/README.md
@@ -0,0 +1,3 @@
+# Weather Observation Station 9
+
+You can find the problem statement [here](https://www.hackerrank.com/challenges/weather-observation-station-9/problem).
---
+++ Easy/13- Weather Observation Station 9/solution.sql
@@ -0,0 +1,3 @@
+Select DISTINCT CITY 
+From STATION
+where (CITY not like "a%" And CITY not like "e%" And CITY not like "i%" And CITY not like "o%" And CITY not like "u%");
