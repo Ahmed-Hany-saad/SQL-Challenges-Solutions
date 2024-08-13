50102bc9ecd692013911931c1d9ab4e044b257e7
Ahmed-Hany-saad <105203723+Ahmed-Hany-saad@users.noreply.github.com>
2024-08-13 22:19:20 +0300

    Update comments.sql

--- comments.sql
+++ comments.sql
@@ -1,15 +1,5 @@
-SELECT
-  usr.nome as usuario,
-  (CASE
-    when year(MAX(hst.data_reproducao)) = 2021 then 'Usuário ativo'
-    else 'Usuário inativo'
-  END) as condicao_usuario
-FROM
-  usuario as usr
-INNER JOIN
-  historico as hst
-ON
-  hst.usuario_id = usr.id
-GROUP BY
-  usr.nome;
+-- 15- Weather Observation Station 11
+-- 16- Weather Observation Station 12
 
+-- 32- Weather Observation Station 15
+-- 33- Weather Observation Station 16
